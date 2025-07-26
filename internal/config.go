50e170c81b9571821911695dfc1742835a8224b7
Mikhail Gerasimov <m.gerasimov@spacevm.org>
2025-07-26 11:01:09 +0300

    add docker + db

--- internal/config.go
+++ internal/config.go
@@ -7,9 +7,10 @@ import (
 )
 
 type Config struct {
-	Addr  string
-	Port  int
-	DBDSN string
+	Addr        string
+	Port        int
+	DBDSN       string
+	MigratePath string
 }
 
 const (
@@ -24,8 +25,11 @@ func ReadConfig() *Config {
 	flag.StringVar(&cfg.Addr, "addr", defaultAddr, "flag for use custom server addr")
 	flag.IntVar(&cfg.Port, "port", defaultPort, "flag for use custom server port")
 	flag.StringVar(&cfg.DBDSN, "db", defaultDBStr, "flag for setup db connection string")
+	flag.StringVar(&cfg.MigratePath, "migrate", "migrations", "flag for setup path to migrations")
 	flag.Parse()
 
 	cfg.DBDSN = cmp.Or(os.Getenv("DB_DSN"), cfg.DBDSN)
+	cfg.MigratePath = cmp.Or(os.Getenv("MIGRATE_PATH"), cfg.MigratePath)
+
 	return &cfg
 }
--- internal/repository/db-storage/storage.go
+++ internal/repository/db-storage/storage.go
@@ -2,6 +2,7 @@ package dbstorage
 
 import (
 	"context"
+	"log"
 	"time"
 
 	"github.com/Dorrrke/library0706/internal/domain/models"
@@ -56,6 +57,37 @@ func (s *Storage) SaveBook(book models.Book) error {
 	}
 
 	return nil
+}
+
+func (s *Storage) SaveBooks(books []models.Book) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	tx, err := s.conn.Begin(ctx)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		err := tx.Rollback(ctx)
+		if err != nil {
+			log.Println("Error rollback: ", err.Error())
+		}
+	}()
+
+	_, err = tx.Prepare(ctx, "save_book", "INSERT INTO books (bid, author, lable, description, genre, writed_at, count) VALUES ($1, $2, $3, $4, $5, $6, $7)")
+	if err != nil {
+		return err
+	}
+
+	for _, book := range books {
+		_, err = tx.Exec(ctx, "save_book", book.BookID, book.Author, book.Lable, book.Description, book.Genre, book.WritedAt, book.Count)
+		if err != nil {
+			return err
+		}
+	}
+
+	return tx.Commit(ctx)
 }
 
 func (s *Storage) GetUser(email string) (models.User, error) {
